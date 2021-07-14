026547084ec6e7087c31e8dee17bd829c95ed0d1
Guy <Ofek.r.guy@gmail.com>
2021-07-15 01:45:25 +0300

    YESSSSS
    it fucking solves it ohmygod haha i am a fucking rust god bitches

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,48 @@
-use std::time::{Duration, SystemTime};
+use std::{time::{SystemTime},ops,fmt};
 
 fn main() {
 
-    let sys_time=SystemTime::now();
-    let res = find_options(&vec![1,1,1,1]);
-    println!("{}",res.len());
-    println!("{:?}",sys_time.elapsed());
-    
+    let start = SystemTime::now();
+    let b = Board::default();
+    for i in 0..100{
+        b.solve();
+
+    }
+    println!("{:?}",start.elapsed());
 }
 
-const SIZE: usize = 100;
+const SIZE: usize = 21;
 
-const WHITE:char=' ';
-const BLACK:char='x';
-const UNKNOWN:char='?';
+#[derive(Clone, Copy,Eq,PartialEq)]
+enum Tile {
+    Black,
+    White,
+    Unknown,
+}
+
+impl fmt::Debug for Tile {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let tile = match self{
+            Tile::Black=>{'■'},
+            Tile::White=>{' '},
+            Tile::Unknown=>{'?'},
+        };
+        write!(f, "{}",tile)
+    }
+}
 
 
+impl ops::Add<Tile> for &Tile{
+    type Output=Tile;
+    fn add(self,rhs:Tile)->Tile{
+        match self{
+            Tile::Unknown=>{Tile::Unknown},
+            _=>{if *self==rhs {*self} else {Tile::Unknown}}
+        }
+    }
+}
 
-type Tile=char;
-
-
-type Line=[Tile;SIZE];
+type Line = [Tile; SIZE];
 
 type Header = Vec<usize>;
 
@@ -31,6 +53,69 @@ struct Board {
     row_nums: SideHeader,
     col_nums: SideHeader,
 }
+
+type Options = Vec<Line>;
+
+
+impl Board{
+    fn solve(&self){
+        let mut row_options :Vec<Options>= self.row_nums.iter().map(find_options).collect();
+        let mut col_options :Vec<Options>= self.col_nums.iter().map(find_options).collect();
+        let mut row_summaries :Options = row_options.iter().map(|x|summarize(&x)).collect();
+        let mut col_summaries :Options = col_options.iter().map(|x|summarize(&x)).collect();
+        
+        while !done(&row_summaries) {
+            filter(&mut row_summaries,&mut col_options);
+            filter(&mut col_summaries,&mut row_options);
+            row_summaries = row_options.iter().map(|x|summarize(&x)).collect();
+            col_summaries = col_options.iter().map(|x|summarize(&x)).collect();
+        }
+
+
+        // for row in row_summaries.iter(){
+        //     println!("{:?}",row);
+        // }
+        // println!();
+
+    }
+}
+
+fn filter(from:&mut Options,to:&mut Vec<Options>){
+    for (r,row) in from.iter().enumerate(){
+        for (t,tile) in row.iter().enumerate(){
+            let mut to_delete :Vec<usize> =Vec::new(); 
+            match tile{
+                Tile::Unknown=>(),
+                _=>{
+                    for (o,option_col) in to[t].iter().enumerate(){
+                        if option_col[r]!=*tile{
+                            to_delete.push(o)
+                        }
+                    }
+                }
+            };
+            to_delete.sort();
+            for del in to_delete.iter().rev(){
+                to[t].remove(*del);
+            }
+            
+        }
+    }
+}
+
+
+fn done(lines:&Options)->bool{
+    for line in lines.iter(){
+        for square in line.iter(){
+            match square{
+                Tile::Unknown=>{return false},
+                _=>()
+            }
+        }
+    }
+    true
+}
+
 
 
 fn min_size(header: &Header) -> usize {
@@ -45,31 +130,98 @@ fn min_size(header: &Header) -> usize {
     }
 }
 
-fn find_options(header: &Header) ->Vec<Line>{
+fn find_options(header: &Header) -> Options {
     let mut res = Vec::<Line>::new();
-    rec_find_options(header, [WHITE;SIZE], 0,&mut res);
+    rec_find_options(header, [Tile::White; SIZE], 0, &mut res);
     res
 }
 
-fn rec_find_options(header: &Header, line: Line, filled:usize, result: &mut Vec<Line>) {
-    if header.len()>0{
+fn rec_find_options(header: &Header, line: Line, filled: usize, result: &mut Options) {
+    if header.len() > 0 {
         let mut remaining = header.to_vec();
         remaining.remove(0);
-        let available = SIZE-filled-min_size(&remaining);
-        let space = if remaining.len()==0 {header[0]} else {header[0]+1};
-        let range = available-space;
-        for i in filled..filled+range+1{
+        let available = SIZE - filled - min_size(&remaining);
+        let space = if remaining.len() == 0 {
+            header[0]
+        } else {
+            header[0] + 1
+        };
+        let range = available - space;
+        for i in filled..filled + range + 1 {
             let mut copy = line;
-            for j in filled..i{
-                copy[j]=WHITE;
+            for j in filled..i {
+                copy[j] = Tile::White;
             }
-            for j in i..i+header[0]{
-                copy[j]=BLACK;
+            for j in i..i + header[0] {
+                copy[j] = Tile::Black;
             }
-            rec_find_options(&remaining, copy, i+space,result);
+            rec_find_options(&remaining, copy, i + space, result);
+        }
+    } else {
+        result.push(line);
+    }
+}
+
+fn summarize(options:&Options)->Line{
+    let mut res = options[0];
+    for line in options.iter(){
+        for (i,tile) in line.iter().enumerate(){
+            res[i]=tile+res[i];
         }
     }
-    else{
-        result.push(line);
+    res
+}
+
+
+impl Default for Board {
+    fn default() -> Self {
+        Self {
+            row_nums: [
+                vec![7, 2, 2, 7],
+                vec![1, 1, 1, 2, 1, 1],
+                vec![1, 3, 1, 3, 1, 1, 3, 1],
+                vec![1, 3, 1, 2, 1, 1, 3, 1],
+                vec![1, 3, 1, 2, 1, 3, 1],
+                vec![1, 1, 2, 2, 1, 1],
+                vec![7, 1, 1, 1, 7],
+                vec![2],
+                vec![2, 3, 2, 1, 4],
+                vec![1, 1, 3, 3, 2, 1],
+                vec![3, 1, 3, 2, 2],
+                vec![1, 1, 1, 3, 1, 1],
+                vec![1, 5, 1, 1, 1, 1],
+                vec![1, 1, 1, 1, 3, 1],
+                vec![7, 1, 1],
+                vec![1, 1, 1, 1, 1, 1, 1, 1],
+                vec![1, 3, 1, 1, 1, 2, 2],
+                vec![1, 3, 1, 2, 1, 2, 1, 1],
+                vec![1, 3, 1, 1, 1, 2],
+                vec![1, 1, 2, 1, 1],
+                vec![7, 1, 3, 1],
+            ],
+            col_nums: [
+                vec![7, 1, 2, 7],
+                vec![1, 1, 1, 1, 1, 1],
+                vec![1, 3, 1, 1, 1, 3, 1],
+                vec![1, 3, 1, 1, 1, 1, 3, 1],
+                vec![1, 3, 1, 1, 1, 1, 3, 1],
+                vec![1, 1, 2, 1, 1],
+                vec![7, 1, 1, 1, 7],
+                vec![4],
+                vec![4, 2, 2, 2, 2, 2],
+                vec![1, 2, 1, 1, 1, 2, 3],
+                vec![1, 2, 2, 2],
+                vec![2, 3, 1, 1, 1, 1, 1],
+                vec![3, 3, 2, 3, 1, 1],
+                vec![1, 1, 3, 2],
+                vec![7, 1, 1],
+                vec![1, 1, 1, 1, 1, 1, 1],
+                vec![1, 3, 1, 3, 2, 3],
+                vec![1, 3, 1, 2, 2, 1, 1],
+                vec![1, 3, 1, 1, 1, 1, 1],
+                vec![1, 1, 5, 3],
+                vec![7, 1, 1, 2, 1],
+            ],
+        }
     }
 }
